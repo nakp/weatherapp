dcf6d43d5358eb741c7a4e9bc6f1b67493e600e4
Tomas Figueroa <teft90@gmail.com>
2016-04-07 16:37:21 -0600

    Initial Commit

---
+++ Weather App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weather App
+//
+//  Created by Tomás Figueroa on 4/7/16.
+//  Copyright © 2016 Applaudo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weather App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weather App
+//
+//  Created by Tomás Figueroa on 4/7/16.
+//  Copyright © 2016 Applaudo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
